6ed07ba8c409e6ee415e3e7bd87a4281e5a7ac65
Daniel Adu-Djan <adudjan.danieladd@gmail.com>
2019-03-22 15:26:14 -0400

    Added simplify and simplified functions, made chages to how __eq__() is implemented.

--- fraction.py
+++ fraction.py
@@ -21,15 +21,50 @@ class Fraction():
         return Fraction(new_numerator, new_denominator)
 
     def __eq__(self, next_fraction):
-        multiplier = next_fraction.a / self.a
-        new_numerator = round(self.a * multiplier, 6)
-        new_denominator = round(self.b * multiplier, 6)
-        return new_numerator == next_fraction.a and\
-            new_denominator == next_fraction.b
+        return self.simplified().__str__() == next_fraction.simplified().__str__()
+            
+    def simplify(self):
+        a = self.a
+        b= self.b
+        while True:
+            if a == b:
+                gcd = a
+                break
+            elif (a == 0 and b) or (a and b == 0):
+                gcd = b if b else a
+                break
+            elif a > b:
+                a %= b
+            else:
+                b %= a
+        
+        self.a = int(self.a / gcd)
+        self.b = int(self.b / gcd)
+    
+    def simplified(self):
+        a = self.a
+        b= self.b
+        while True:
+            if a == b:
+                gcd = a
+                break
+            elif (a == 0 and b) or (a and b == 0):
+                gcd = b if b else a
+                break
+            elif a > b:
+                a %= b
+            else:
+                b %= a
+        
+        a = int(self.a / gcd)
+        b = int(self.b / gcd)
+        
+        return Fraction(a,b)
 
-#f1 = Fraction(1,3)
+#af1 = Fraction(1,3)
 #f2 = Fraction(2,5)
 #f3 = Fraction(9,27)
+
 
 #f4 = f1 + f2
 #f5 = f1 - f2
@@ -39,3 +74,7 @@ class Fraction():
 #print(f1,'+',f2, '=', f4)
 #print(f1,'-',f2, '=', f5)
 #print(f1 == f3)
+#print(f3)
+#print(f3.simplified())
+#print(f3)
+
